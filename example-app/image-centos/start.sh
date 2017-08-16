bd115540f5d0445f80c40947c48812101bfb2455
Dan Isla <disla@google.com>
2017-08-16 15:15:18 -0700

    update to example app

--- example-app/image-centos/start.sh
+++ example-app/image-centos/start.sh
@@ -9,8 +9,9 @@ trap _term SIGTERM
 
 # main loop
 (while true; do
-    echo "`date -u` External IP: `curl -s https://api.ipify.org`"
-    echo "`date -u` GCS Bucket object content length: `curl -sI https://storage.googleapis.com/solutions-public-assets/adtech/dfp_networkimpressions.py |grep -i x-goog-stored-content-length`"
+    echo "`date -u` https://www.google.com: `curl -s -o /dev/null -w '%{http_code}' https://www.google.com`"
+    echo "`date -u` https://storage.googleapis.com/solutions-public-assets/: `curl -s -o /dev/null -w '%{http_code}' https://storage.googleapis.com/solutions-public-assets/adtech/dfp_networkimpressions.py`"
+    echo "`date -u` `ping -c1 www.google.com 2>&1`"
     sleep 5
 done) &
 
