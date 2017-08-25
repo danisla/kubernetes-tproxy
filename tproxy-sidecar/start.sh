e67f4fc1138395737f4a92c1c29333dc9253bced
Dan Isla <disla@google.com>
2017-08-24 19:01:36 -0700

    added BLOCK_SVC_CIDR and ALLOW_DNS vars

--- tproxy-sidecar/start.sh
+++ tproxy-sidecar/start.sh
@@ -6,6 +6,13 @@ set -eo pipefail
 export POD_IP=$(hostname -i)
 export POD_NAME=$(hostname)
 
+# dns allow
+[[ -n "${ALLOW_DNS}" ]] && iptables -w -t filter -A OUTPUT -p tcp -m tcp --dport 53 --destination ${ALLOW_DNS} -j ACCEPT
+[[ -n "${ALLOW_DNS}" ]] && iptables -w -t filter -A OUTPUT -p udp -m udp --dport 53 --destination ${ALLOW_DNS} -j ACCEPT
+
+# service network deny
+[[ -n "${BLOCK_SVC_CIDR}" ]] && iptables -w -t filter -A OUTPUT --destination ${BLOCK_SVC_CIDR} -j REJECT
+
 # pod egress deny
 iptables -w -t filter -A OUTPUT -p tcp -m tcp --dport 53 -j ACCEPT
 iptables -w -t filter -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
