1d43cc803bc0aefef35671bd0c268715908648aa
Dan Isla <disla@google.com>
2017-08-25 18:49:49 -0700

    migrate from HOST_IP to NODE_NAME for k8s 1.6 compat

--- tproxy-sidecar/start.sh
+++ tproxy-sidecar/start.sh
@@ -18,5 +18,11 @@ iptables -w -t filter -A OUTPUT -p tcp -m tcp --dport 53 -j ACCEPT
 iptables -w -t filter -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
 iptables -w -t filter -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
 iptables -w -t filter -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
-[[ -n "${HOST_IP}" ]] && iptables -w -t filter -I OUTPUT -p tcp -m tcp --dport 1080 --destination ${HOST_IP} -j ACCEPT
+
+if [[ -n "${NODE_NAME}" ]]; then
+  HOST_IP=$(getent hosts ${NODE_NAME} | awk '{print $1}')
+  [[ ! "${HOST_IP}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && echo "ERROR: Could not get IP from node name" && exit 1
+  iptables -w -t filter -I OUTPUT -p tcp -m tcp --dport 1080 --destination ${HOST_IP} -j ACCEPT
+fi
+
 iptables -w -t filter -A OUTPUT -j REJECT
